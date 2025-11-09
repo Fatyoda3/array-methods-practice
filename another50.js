206f23b993cc1b4283f6ccc1c132e9a9bd896982
Fatyoda3 <shivangsingh654@gmail.com>
2025-11-09 22:37:15 +0530

    `just finished `

--- another50.js
+++ another50.js
@@ -513,6 +513,18 @@ console.log("99.", a99);
 
 // ### 100. Distinct Hummed Songs
 // Create a list of distinct songs hummed by children on a bus ride.
-const q100 = 
-const a100 = q100
+const q100 = [
+  ["Hmm hmm", "La la", "Na na", "Oh oh"],
+  ["Da da", "Na na", "Hmm hmm", "Ba ba"],
+  ["La la", "Oo oo", "Mm mm", "Na na"],
+  ["Oh oh", "Ha ha", "Hmm hmm", "La la"],
+  ["Na na", "Da da", "La la", "Yeah yeah"]
+];
+
+const a100 = q100.flat().reduce((distinct, current) => {
+  if (!distinct.includes(current)) {
+    distinct.push(current);
+  }
+  return distinct;
+}, []);
 console.log("100.", a100);
